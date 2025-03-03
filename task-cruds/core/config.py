130b6bcc0d9b1038fff2f5419e08b036ba689d46
Максим <twinkingdase@gmail.com>
2025-03-03 22:05:53 +0300

    Создана папка core/models, настроен config.py под будущий apiv1, и настроена директория под aiosqlite.

---
+++ task-cruds/core/config.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+    db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
+    db_echo: bool = True
