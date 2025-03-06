cf9406dd4c10b2fdd51744be5d3b09ba8ae40865
Максим <twinkingdase@gmail.com>
2025-03-06 23:13:17 +0300

    еще изменения моделей бд

--- task-cruds/core/models/task.py
+++ task-cruds/core/models/task.py
@@ -13,4 +13,15 @@ class Task(Base):
     task_importance: Mapped[int] = mapped_column(Integer, nullable=False)
     task_created_date: Mapped[str] = mapped_column(String(50), nullable=False)
     task_created_until_date: Mapped[str] = mapped_column(String(50), nullable=True)
+    task_color: Mapped[str] = mapped_column(String(7), nullable=False, default="#0E0E10", server_default="#0E0E10")
+    is_archived: Mapped[int] = mapped_column(Integer, default=0)
+
+
+class NewTable(Base):
+    __tablename__ = "habits"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    habit_title: Mapped[str] = mapped_column(String(255), nullable=False)
+    habit_counter = Mapped[int] = mapped_column(Integer, nullable=False)
+    habit_countet = Mapped[str] = mapped_column(String(7), nullable=False, default="#0E0E10", server_default="#0E0E10")
     is_archived: Mapped[int] = mapped_column(Integer, default=0)
