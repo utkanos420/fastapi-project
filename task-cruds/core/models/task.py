54b6533ab6b6755b7d9bfac7d04d0b5897664f92
Максим <twinkingdase@gmail.com>
2025-03-06 23:04:12 +0300

    изменена модель таблицы tasks

--- task-cruds/core/models/task.py
+++ task-cruds/core/models/task.py
@@ -7,11 +7,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Task(Base):
     __tablename__ = "tasks"
 
-
-    task_title: Mapped[str]
-    task_description: Mapped[str]
-    task_importance: Mapped[int]
-    task_created_date: Mapped[str]
-    task_created_until_date: Mapped[str]
-
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)  
+    task_title: Mapped[str] = mapped_column(String(255), nullable=False)
+    task_description: Mapped[str] = mapped_column(String(500), nullable=True)
+    task_importance: Mapped[int] = mapped_column(Integer, nullable=False)
+    task_created_date: Mapped[str] = mapped_column(String(50), nullable=False)
+    task_created_until_date: Mapped[str] = mapped_column(String(50), nullable=True)
     is_archived: Mapped[int] = mapped_column(Integer, default=0)
